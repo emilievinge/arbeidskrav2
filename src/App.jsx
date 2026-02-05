fcef93fe05eca4264ee2321a0b09b9e31178a6cb
emilievinge <emiliev@hiof.no>
2026-02-05 16:18:43 +0100

    Startet filer :t-rex:

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@
 function App() {
   const [count, setCount] = useState(0)
 
+  function Header() {
+    return (
+      <header>
+        <h1>Handleliste</h1>
+      </header>
+    )
+  }
+
+  function Main() {
+    return (
+      <main></main>
+    )
+  }
+
   return (
     <>
-
+    <div id="container">
+      <Header/>
+    </div>
     </>
   )
 }
