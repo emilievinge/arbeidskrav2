4db8590f71df077bf7c442c2fce8b3e55cf7b0e4
emilievinge <emiliev@hiof.no>
2026-02-10 13:06:22 +0100

    html ish ferdig til arbeidskrav 2 :t-rex:

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
+import './style/layout.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
 
   function Header() {
     return (
@@ -10,17 +13,43 @@ function App() {
     )
   }
 
-  function Main() {
-    return (
-      <main></main>
+  function AddForm() {
+    return(
+      <form>
+        <label htmlFor="item">Vare</label>
+        <input id="item" type="text" placeholder="Havregryn" />
+        <label htmlFor="quantity">Antall</label>
+        <input id="quantity" type="number" min="1" max="100" placeholder="1" />
+        <button id="addItem">Legg til vare</button>
+      </form>
+    )
+  }
+
+  function ShoppingList() {
+    return(
+      <ul>
+        <li>
+          <input id="checkbox" type="checkbox" checked />
+          <label htmlFor="checkbox">Melk</label>
+          <input type="number" min="1" max="100" placeholder="1" />
+        </li>
+        <li>
+          <input id="checkbox" type="checkbox" />
+          <label htmlFor="checkbox">Egg</label>
+          <input type="number" min="1" max="100" placeholder="1" />
+        </li>
+      </ul> 
     )
   }
 
   return (
     <>
-    <div id="container">
-      <Header/>
-    </div>
+      <Header />
+      <main>
+        <AddForm />
+        <ShoppingList />
+        
+      </main>
     </>
   )
 }
