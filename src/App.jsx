d8a8acc9a1dc534a4251148af6aae73dc3971d06
emilievinge <emiliev@hiof.no>
2026-02-11 16:32:28 +0100

    funksjoner bleh

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import './style/layout.css'
 function App() {
   const [count, setCount] = useState(0)
 
-
   function Header() {
     return (
       <header>
@@ -13,14 +12,28 @@ function App() {
     )
   }
 
-  function AddForm() {
-    return(
-      <form>
-        <label htmlFor="item">Vare</label>
-        <input id="item" type="text" placeholder="Havregryn" />
-        <label htmlFor="quantity">Antall</label>
-        <input id="quantity" type="number" min="1" max="100" placeholder="1" />
-        <button id="addItem">Legg til vare</button>
+  function AddForm({form, setForm, setShoppinglist}) {
+    const handleChange = (e) => {
+
+      const { name, value } = e.target
+      setForm((prev) => ({...prev, [name]: value})
+      )
+    }
+
+    const handleClick = (e)=>{
+      e.preventDefault()
+      const uniqId = crypto.randomUUID()
+      setShoppinglist((prev) => ([...prev, {id:uniqId,...form}]))
+      console.log(form)
+    }
+
+    return (
+      <form className="addForm" onSubmit={handleClick}>
+          <label htmlFor="item">Vare</label>
+          <input id="item" type="text" name="item" placeholder="Havregryn" onChange={handleChange}/>
+          <label htmlFor="quantity">Antall</label>
+          <input id="quantity" type="number" min="1" max="100" name="quantity" placeholder="1" onChange={handleChange} />
+          <button>Legg til vare</button>
       </form>
     )
   }
@@ -42,13 +55,27 @@ function App() {
     )
   }
 
+  function ShoppingItem({title, content, id, setTodoList}){
+    const handleClick = ()=>{
+        setTodoList((prev) => prev.filter(item => item.id !== id))
+        console.log(id)
+    }
+    return (
+        <article className="todo-card">
+            <h3>{title}</h3>
+            <p>{content}</p>
+            <button onClick={handleClick}>Done</button>
+        </article>
+    )
+  }
+
   return (
     <>
       <Header />
       <main>
         <AddForm />
         <ShoppingList />
-        
+        <ShoppingItem />
       </main>
     </>
   )
